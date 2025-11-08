2a5e6aa51378f811622b9dfcf24d414fc3028d4c
NextoNexus <3192292489@qq.com>
2025-11-08 18:38:16 +0800

    ``` feat(utils): 添加合并两个数字列表的工具函数
    新增 catLists 函数用于连接两个数字数组，返回一个新的合并后的数组。
    该函数接受两个参数 list1 和 list2，均为 number[] 类型。
    ```

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,2 +1,5 @@
 export const mergeName = (firstName: string, lastName: string) =>
   `${firstName} ${lastName}`;
+export const catLists = (list1: number[], list2: number[])=>{
+  return list1.concat(list2);
+};
